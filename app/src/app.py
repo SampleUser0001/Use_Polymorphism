92198f811a95d6b5e8e80cbf0dcbd353a136c241
Satoru Tanaka <ittimfn@gmail.com>
2021-05-30 20:02:20 +0900

    戻り値の型を変えてみる

--- app/src/app.py
+++ app/src/app.py
@@ -10,3 +10,7 @@ if __name__ == '__main__':
 
   sub01.print_value()
   sub02.print_value()
+
+  # 戻り値の型が異なるが、動く。
+  print(sub01.get_hoge_type())
+  print(sub02.get_hoge_type())
--- app/src/classes.py
+++ app/src/classes.py
@@ -16,6 +16,11 @@ class SampleSuper(metaclass=ABCMeta):
     """ 何らかの文字列と一緒にvalueを出力する。 """
     pass
 
+  @abstractmethod
+  def get_hoge_type():
+    """ サブクラス側で戻り値の型が異なっていても、動くか """ 
+    pass
+
 class SampleSub01(SampleSuper):
   def __init__(self, value):
     super().__init__(value)
@@ -23,9 +28,15 @@ class SampleSub01(SampleSuper):
   def print_value(self):
     print("Sub01.value: {}".format(self.get_value()))
 
+  def get_hoge_type(self):
+    return [self.value]
+
 class SampleSub02(SampleSuper):
   def __init__(self, value):
     super().__init__(value)
 
   def print_value(self):
     print("Sub02.value: {}".format(self.get_value()))
+
+  def get_hoge_type(self):
+    return {'key' :self.value}
